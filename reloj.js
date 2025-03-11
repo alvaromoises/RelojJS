e6d6daf97999c13a850e239053d9f7cda06dcb4d
Alvaro <ap19371937@gmail.com>
2025-03-11 11:44:25 -0500

    agregacion de fecha

--- reloj.js
+++ reloj.js
@@ -5,6 +5,7 @@ console.log(document)
 const horas = document.getElementById('horas')
 const minutos = document.getElementById('minutos')
 const segundos = document.getElementById('segundos')
+const fecha = document.getElementById('fecha')
 
 
 function reloj() {
@@ -13,10 +14,12 @@ function reloj() {
         horas.textContent = date.getHours().toString().padStart(2, 0)
         minutos.textContent = date.getMinutes().toString().padStart(2, 0)
         segundos.textContent = date.getSeconds().toString().padStart(2, 0)
+        let meses = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+        let dia = `${date.getDate().toString().padStart(2, 0)} ${meses[date.getMonth()]} ${date.getFullYear()}`
+        fecha.textContent = dia
     } catch (error) {
         console.log(error)
     }
 
 }
 setInterval(reloj, 100)
-
